55b8164e0861242d165b33e40272add1f3cc82a6
tangjiale <jialesmile@foxmail.com>
2021-09-28 10:56:07 +0800

    增加ios bark 消息推送功能

--- push_message.py
+++ push_message.py
@@ -25,4 +25,23 @@ def push_wx_message(uid, message):
     response = requests.post(url=push_url, json=req_json, headers=header)
     print(response.text)
 
+
+# ios bark 消息通知推送（只能推送ios设置，并必须安装bark应用）
+# @param uid 用户唯一key
+# @param title 通知消息标题
+# @param message 通知消息正文
+def push_ios_bark_message(uid, title, message, category):
+    push_url = "https://api.day.app/%s" % uid
+    req_json = {
+        "title": title,
+        "body": message,
+        "category": category
+    }
+    header = {
+        "Content-Type": "application/json"
+    }
+    response = requests.post(url=push_url, json=req_json, headers=header)
+    print(response.text)
+
 # stt.push_message("UID_qNCI9uES2zahjmm8W3iGZAEB07sv", "您似乎已经签到过了...")
+# push_ios_bark_message("PBF2K8wMRPdj5bKhcdiy6G", "管理圈", "您似乎已经签到过了...","自动签到")
